0d6f4baabd9b9a6ccabbfd5b8fba22fcd8001669
Riley Rangel <rrangel91@gmail.com>
2017-09-29 11:17:18 -0700

    Add turn, accelerate, move and start methods to class. #2

--- main.js
+++ main.js
@@ -14,6 +14,34 @@ class Car {
     this.speed = speed
     this.location = location
   }
+
+  turn(direction) {
+    this.direction = direction
+  }
+
+  accelerate(speed) {
+    this.speed += speed
+  }
+
+  move() {
+    switch (this.direction) {
+      case 'North':
+        this.location[1] += this.speed
+        break
+      case 'East':
+        this.location[0] += this.speed
+        break
+      case 'South':
+        this.location[1] -= this.speed
+        break
+      case 'West':
+        this.location[0] -= this.speed
+    }
+  }
+
+  static start(car) {
+    setInterval(car.move(), 16)
+  }
 }
 
 console.log(Car)
